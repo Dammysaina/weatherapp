362167b7ce9c9328728b5a26dc4dd842249b058e
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2021-11-18 13:43:29 +0100

    next

--- src/components/Next/Next.js
+++ src/components/Next/Next.js
@@ -1,10 +1,23 @@
 import React, {Component} from "react";
+import "./Next.css"
+// import Button from "../../library/Button.styles";
 
 export default class Next extends Component {
     render () {
         return (
-            <div>
-                <p>hi</p>
+            <div className="nextFiveDays">
+                <div className="fiveDays">
+                    <p>Next Forecast</p>
+                    <p className="dayFive">Five Days</p>
+                </div>
+                <div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
             </div>
         )
     }
---
+++ src/components/Next/Nextcard.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+export default class Nextcard extends Component {
+    render () {
+        return (
+            <div>
+                <div>
+                    <p>April 5 </p>
+                    <img src="../images/Sun.svg" alt="Sun"/>
+                    <p>8 a.m</p>
+                </div>
+            </div>
+        )
+    }
+}
